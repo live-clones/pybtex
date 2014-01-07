9abcb78fbc79a89316ee8b42c0e15aeced47f757
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2014-01-07 13:27:36 +0000

    Simplify plugin implementation to use entry points only.

--- pybtex/plugin/__init__.py
+++ pybtex/plugin/__init__.py
@@ -20,7 +20,11 @@
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
-from pybtex.plugin.loader import PluginLoader
+import collections
+import os.path  # splitext
+import pkg_resources
+
+from pybtex.exceptions import PybtexError
 
 
 class Plugin(object):
@@ -36,7 +40,117 @@ class Plugin(object):
             return None
 
 
-plugin_loader = PluginLoader()
-find_plugin = plugin_loader.find_plugin
-enumerate_plugin_names = plugin_loader.enumerate_plugin_names
-register_plugin = plugin_loader.register_plugin
+#: default pybtex plugins
+_DEFAULT_PLUGINS = {
+    "pybtex.database.input": "bibtex",
+    "pybtex.database.output": "bibtex",
+    "pybtex.backends": "latex",
+    "pybtex.style.labels": "number",
+    "pybtex.style.names": "plain",
+    "pybtex.style.sorting": "none",
+    "pybtex.style.formatting": "unsrt",
+    }
+
+
+class PluginGroupNotFound(PybtexError):
+    def __init__(self, group_name):
+        message = u'plugin group {group_name} not found'.format(
+            group_name=group_name,
+        )
+        super(PluginGroupNotFound, self).__init__(message)
+
+
+class PluginNotFound(PybtexError):
+    def __init__(self, plugin_group, name):
+        if not name.startswith('.'):
+            message = u'plugin {plugin_group}.{name} not found'.format(
+                plugin_group=plugin_group,
+                name=name,
+            )
+        else:
+            assert plugin_group.endswith('.suffixes')
+            message = u'plugin {plugin_group} for suffix {suffix} not found'.format(
+                plugin_group=plugin_group,
+                suffix=name,
+            )
+
+        super(PluginNotFound, self).__init__(message)
+
+
+def _load_entry_point(group, name):
+    entry_points = pkg_resources.iter_entry_points(group, name)
+    try:
+        entry_point = entry_points.next()
+    except StopIteration:
+        raise PluginNotFound(group, name)
+    else:
+        return entry_point.load()
+
+def find_plugin(plugin_group, name=None, filename=None):
+    """Find a :class:`Plugin` class within *plugin_group* which
+    matches *name*, or *filename* if *name* is not specified, or
+    the default plugin if neither *name* nor *filename* is
+    specified.
+
+    If *name* is specified, return the :class:`Plugin` class
+    registered under *name*. If *filename* is specified, look at
+    its suffix (i.e. extension) and return the :class:`Plugin`
+    class registered for this suffix.
+    """
+    if plugin_group not in _DEFAULT_PLUGINS:
+        raise PluginGroupNotFound(plugin_group)
+    if name:
+        return _load_entry_point(plugin_group, name)
+    elif filename:
+        suffix = os.path.splitext(filename)[1]
+        return _load_entry_point(plugin_group + '.suffixes', suffix)
+    else:
+        return _load_entry_point(plugin_group, _DEFAULT_PLUGINS[plugin_group])
+
+def enumerate_plugin_names(plugin_group):
+    """Enumerate all plugin names for the given *plugin_group*."""
+    return (entry_point.name
+            for entry_point in pkg_resources.iter_entry_points(plugin_group))
+
+class FakeEntryPoint(pkg_resources.EntryPoint):
+    def __init__(self, name, klass):
+        self.name = name
+        self.klass = klass
+
+    def __str__(self):
+        return "%s = :%s" % (self.name, self.klass.__name__)
+
+    def __repr__(self):
+        return (
+            "FakeEntryPoint(name=%r, klass=%s)"
+            % (self.name, self.klass.__name__))
+
+    def load(self, require=True, env=None, installer=None):
+        return self.klass
+
+    def require(self, env=None, installer=None):
+        pass
+
+
+def register_plugin(plugin_group, name, klass):
+    if not plugin_group.endswith(".suffixes"):
+        # registering a name
+        if plugin_group not in _DEFAULT_PLUGINS:
+            raise PluginGroupNotFound(plugin_group)
+    else:
+        # registering a suffix
+        group, _1, _2 = plugin_group.rpartition(".suffixes")
+        assert _1 == ".suffixes" and _2 == ""
+        if not name.startswith('.'):
+            raise ValueError("a suffix must start with a period")
+        if group not in _DEFAULT_PLUGINS:
+            raise PluginGroupNotFound(group)
+    dist = pkg_resources.get_distribution('pybtex')
+    ep_map = pkg_resources.get_entry_map(dist)
+    if plugin_group not in ep_map:
+        ep_map[plugin_group] = {}
+    if name in ep_map[plugin_group]:
+        # XXX could also raise exception
+        print "%s already registered in group %s" % (name, plugin_group)
+    else:
+        ep_map[plugin_group][name] = FakeEntryPoint(name, klass)
--- pybtex/tests/plugin_test.py
+++ pybtex/tests/plugin_test.py
@@ -3,87 +3,68 @@ import nose.tools
 
 import pybtex.database.input.bibtex
 import pybtex.plugin
-import pybtex.plugin.loader
 import pybtex.style.formatting.plain
 
 def test_plugin_loader():
     """Check that all enumerated plugins can be imported."""
-    for group in pybtex.plugin.loader.PLUGIN_GROUPS:
+    for group in pybtex.plugin._DEFAULT_PLUGINS:
         for name in pybtex.plugin.enumerate_plugin_names(group):
             pybtex.plugin.find_plugin(group, name)
 
-def test_builtin_plugin_module_class_datas():
-    for module_name, class_name, plugin_data in pybtex.plugin.loader.builtin_plugin_module_class_datas():
-        plugin = getattr(importlib.import_module(module_name), class_name)
-        nose.tools.assert_is(plugin, pybtex.plugin.find_plugin(
-            plugin_data.plugin_group, plugin_data.plugin_name))
-        nose.tools.assert_equal(plugin_data.plugin_name, plugin.name)
-        nose.tools.assert_equal(plugin_data.suffixes, plugin.suffixes)
-        nose.tools.assert_equal(plugin_data.aliases, plugin.aliases)
-
 class TestPlugin1(pybtex.plugin.Plugin):
-    name = 'yippikayee'
+    pass
 
 class TestPlugin2(pybtex.plugin.Plugin):
-    name = 'plain'
+    pass
 
 class TestPlugin3(pybtex.plugin.Plugin):
-    name = 'yippikayeetoo'
-    aliases = ('plain',)
+    pass
 
 def test_register_plugin_1():
-    pybtex.plugin.register_plugin('pybtex.style.formatting', TestPlugin1)
+    pybtex.plugin.register_plugin(
+        'pybtex.style.formatting', 'yippikayee', TestPlugin1)
     nose.tools.assert_is(
         TestPlugin1, pybtex.plugin.find_plugin(
             'pybtex.style.formatting', 'yippikayee'))
 
 def test_register_plugin_2():
-    pybtex.plugin.register_plugin('pybtex.style.formatting', TestPlugin2)
+    pybtex.plugin.register_plugin(
+        'pybtex.style.formatting', 'plain', TestPlugin2)
     plugin = pybtex.plugin.find_plugin('pybtex.style.formatting', 'plain')
     nose.tools.assert_is_not(plugin, TestPlugin2)
     nose.tools.assert_is(plugin, pybtex.style.formatting.plain.Style)
 
 def test_register_plugin_3():
-    pybtex.plugin.register_plugin('pybtex.style.formatting', TestPlugin3)
-    plugin = pybtex.plugin.find_plugin('pybtex.style.formatting', 'plain')
-    nose.tools.assert_is_not(plugin, TestPlugin3)
-    nose.tools.assert_is(plugin, pybtex.style.formatting.plain.Style)
+    pybtex.plugin.register_plugin(
+        'pybtex.style.formatting.suffixes', '.woo', TestPlugin3)
+    plugin = pybtex.plugin.find_plugin(
+        'pybtex.style.formatting', filename='test.woo')
+    nose.tools.assert_is(plugin, TestPlugin3)
 
-def test_bad_plugin():
+def test_bad_find_plugin():
     nose.tools.assert_raises(
-        pybtex.plugin.loader.PluginGroupNotFound,
+        pybtex.plugin.PluginGroupNotFound,
         lambda: pybtex.plugin.find_plugin("pybtex.invalid.group", "__oops"))
     nose.tools.assert_raises(
-        pybtex.plugin.loader.PluginNotFound,
+        pybtex.plugin.PluginNotFound,
         lambda: pybtex.plugin.find_plugin("pybtex.style.formatting", "__oops"))
     nose.tools.assert_raises(
-        pybtex.plugin.loader.PluginNotFound,
+        pybtex.plugin.PluginNotFound,
         lambda: pybtex.plugin.find_plugin("pybtex.style.formatting",
                                           filename="oh.__oops"))
+
+def test_bad_register_plugin():
+    nose.tools.assert_raises(
+        pybtex.plugin.PluginGroupNotFound,
+        lambda: pybtex.plugin.register_plugin(
+            "pybtex.invalid.group", "__oops", TestPlugin1))
+    # suffixes must start with a dot
+    nose.tools.assert_raises(
+        ValueError,
+        lambda: pybtex.plugin.register_plugin(
+            "pybtex.style.formatting.suffixes", "notasuffix", TestPlugin1))
 
 def test_plugin_suffix():
     plugin = pybtex.plugin.find_plugin(
         "pybtex.database.input", filename="test.bib")
     nose.tools.assert_is(plugin, pybtex.database.input.bibtex.Parser)
-
-def test_entry_point():
-    # create artificial entry point and re-register just for testing;
-    # in practice you would not do this in real production code
-    import pkg_resources
-    dist = pkg_resources.get_distribution('pybtex')
-    ep_map = pkg_resources.get_entry_map(dist)
-    assert "pybtex.database.input" not in ep_map
-    ep_map["pybtex.database.input"] = {
-        "woohahaha": pkg_resources.EntryPoint(
-            "woohahaha", "pybtex.database.input.bibtex",
-            attrs=("Parser",), dist=dist)}
-    assert "pybtex.database.input.suffixes" not in ep_map
-    ep_map["pybtex.database.input.suffixes"] = {
-        ".woo": pkg_resources.EntryPoint(
-            ".woo", "pybtex.database.input.bibtex",
-            attrs=("Parser",), dist=dist)}
-    pybtex.plugin.plugin_loader._register_entry_point_plugins()
-    p1 = pybtex.plugin.find_plugin("pybtex.database.input", "woohahaha")
-    nose.tools.assert_is(p1, pybtex.database.input.bibtex.Parser)
-    p2 = pybtex.plugin.find_plugin("pybtex.database.input", filename="t.woo")
-    nose.tools.assert_is(p2, pybtex.database.input.bibtex.Parser)
