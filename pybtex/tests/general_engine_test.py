6dc61a113a0f62bea8c8eb3ffae0b4de8f4586d1
Andrey Golovizin <ag@sologoc.com>
2014-03-25 00:22:08 +0100

    Add support for custom .aux files to bibtex_engine_test.

--- pybtex/tests/general_engine_test.py
+++ pybtex/tests/general_engine_test.py
@@ -5,7 +5,6 @@ from contextlib import contextmanager
 from shutil import rmtree
 from tempfile import mkdtemp
 
-
 from pybtex import io
 from pybtex import errors
 from pybtex.tests import diff
@@ -30,9 +29,9 @@ def copy_resource(package, resource):
         data_file.write(data)
 
 
-def copy_files(bib_name, bst_name):
-    copy_resource('pybtex.tests.data', bib_name + '.bib')
-    copy_resource('pybtex.tests.data', bst_name + '.bst')
+def copy_files(filenames):
+    for filename in filenames:
+        copy_resource('pybtex.tests.data', filename)
 
 
 def write_aux(aux_name, bib_name, bst_name):
@@ -42,15 +41,29 @@ def write_aux(aux_name, bib_name, bst_name):
         aux_file.write(u'\\bibdata{{{0}}}\n'.format(bib_name))
 
 
-def check_make_bibliography(engine, bib_name, bst_name):
+def check_make_bibliography(engine, filenames):
+    allowed_exts = {'.bst', '.bib', '.aux'}
+    filenames_by_ext = dict(
+        (posixpath.splitext(filename)[1], filename) for filename in filenames
+    )
+    engine_name = engine.__name__.rsplit('.', 1)[-1]
+
+    for ext in filenames_by_ext:
+        if ext not in allowed_exts:
+            raise ValueError(ext)
+
     with cd_tempdir() as tempdir:
-        copy_files(bib_name, bst_name)
-        write_aux('test.aux', bib_name, bst_name)
+        copy_files(filenames)
+        bib_name = posixpath.splitext(filenames_by_ext['.bib'])[0]
+        bst_name = posixpath.splitext(filenames_by_ext['.bst'])[0]
+        if not '.aux' in filenames_by_ext:
+            write_aux('test.aux', bib_name, bst_name)
+            filenames_by_ext['.aux'] = 'test.aux'
         with errors.capture() as stderr:  # FIXME check error messages
-            engine.make_bibliography('test.aux')
-        with io.open_unicode('test.bbl', 'r') as result_file:
+            engine.make_bibliography(filenames_by_ext['.aux'])
+        result_name = posixpath.splitext(filenames_by_ext['.aux'])[0] + '.bbl'
+        with io.open_unicode(result_name) as result_file:
             result = result_file.read()
-        engine_name = engine.__name__.rsplit('.', 1)[-1]
         correct_result_name = '{0}_{1}.{2}.bbl'.format(bib_name, bst_name, engine_name)
         correct_result = pkgutil.get_data('pybtex.tests.data', correct_result_name).decode(io.get_default_encoding())
         assert result == correct_result, diff(correct_result, result)
@@ -58,21 +71,21 @@ def check_make_bibliography(engine, bib_name, bst_name):
 
 def test_bibtex_engine():
     from pybtex import bibtex
-    for bib_name, bst_name in [
-        ('xampl', 'unsrt'),
-        ('xampl', 'plain'),
-        ('xampl', 'alpha'),
-        ('cyrillic', 'unsrt'),
-        ('cyrillic', 'alpha'),
+    for filenames in [
+        ('xampl.bib', 'unsrt.bst'),
+        ('xampl.bib', 'plain.bst'),
+        ('xampl.bib', 'alpha.bst'),
+        ('cyrillic.bib', 'unsrt.bst'),
+        ('cyrillic.bib', 'alpha.bst'),
     ]:
-        yield check_make_bibliography, bibtex, bib_name, bst_name
+        yield check_make_bibliography, bibtex, filenames
 
 
 def test_pybte_engine():
     import pybtex
-    for bib_name, bst_name in [
-        ('cyrillic', 'unsrt'),
-        ('cyrillic', 'plain'),
-        ('cyrillic', 'alpha'),
+    for filenames in [
+        ('cyrillic.bib', 'unsrt.bst'),
+        ('cyrillic.bib', 'plain.bst'),
+        ('cyrillic.bib', 'alpha.bst'),
     ]:
-        yield check_make_bibliography, pybtex, bib_name, bst_name
+        yield check_make_bibliography, pybtex, filenames
