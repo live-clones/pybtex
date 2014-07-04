9460722f21bda075bbad0f9c864f9d9b216d9bda
Andrey Golovizin <ag@sologoc.com>
2014-07-05 00:28:56 +0200

    Make CaseInsensitiveSet remember original keys.

--- pybtex/utils.py
+++ pybtex/utils.py
@@ -24,7 +24,7 @@
 
 
 from functools import wraps
-from collections import Sequence, MutableMapping
+from collections import Sequence, MutableMapping, MutableSet
 from types import GeneratorType
 
 
@@ -50,6 +50,14 @@ def memoize(f):
             memory[args] = f(*args)
         return memory[args]
     return new_f
+
+
+class CaseMismatch(ValueError):
+    def __init__(self, existing_key, new_key):
+        self.existing_key = existing_key
+        self.new_key = new_key
+        message = u'Case mismatch: {0} and {1}'.format(existing_key, new_key)
+        super(CaseMismatch, self).__init__(message)
 
 
 class CaseInsensitiveDict(MutableMapping):
@@ -267,7 +275,7 @@ class OrderedCaseInsensitiveDict(CaseInsensitiveDict):
         return [(key, self[key]) for key in self.order]
 
 
-class CaseInsensitiveSet(set):
+class CaseInsensitiveSet(MutableSet):
     """A very basic case-insensitive set.
 
     >>> s = CaseInsensitiveSet()
@@ -276,10 +284,9 @@ class CaseInsensitiveSet(set):
     >>> 'a' in s
     False
 
-    >>> list(CaseInsensitiveSet(['aaa', 'Aaa', 'AAA']))
-    ['aaa']
-
     >>> s = CaseInsensitiveSet(['Aaa', 'Bbb'])
+    >>> s
+    CaseInsensitiveSet(['Aaa', 'Bbb'])
     >>> len(s)
     2
     >>> 'aaa' in s
@@ -292,6 +299,8 @@ class CaseInsensitiveSet(set):
     True
     >>> 'Bbb' in s
     True
+    >>> 'abc' in s
+    False
     >>> s.add('ccc')
     >>> len(s)
     3
@@ -305,17 +314,45 @@ class CaseInsensitiveSet(set):
     >>> 'aaa' in s
     False
 
+    >>> bool(CaseInsensitiveSet(['a']))
+    True
+    >>> bool(CaseInsensitiveSet([]))
+    False
+    >>> bool(CaseInsensitiveSet())
+    False
+
     """
 
-    def __init__(self, *args, **kwargs):
-        initial_data = set(*args, **kwargs)
-        super(CaseInsensitiveSet, self).__init__(item.lower() for item in initial_data)
+    def __init__(self, iterable=()):
+        self._set = set()
+        self._keys = dict()
+        for item in iterable:
+            self.add(item)
 
-    def __contains__(self, item):
-        return super(CaseInsensitiveSet, self).__contains__(item.lower())
+    def __contains__(self, key):
+        return key.lower() in self._set
 
-    def add(self, item):
-        super(CaseInsensitiveSet, self).add(item.lower())
+    def __iter__(self):
+        return __iter__(self._set)
 
-    def remove(self, item):
-        super(CaseInsensitiveSet, self).remove(item.lower())
+    def __len__(self):
+        return len(self._set)
+
+    def __repr__(self):
+        """A caselessDict version of __repr__ """
+        return '{0}({1})'.format(
+            type(self).__name__, repr(sorted(self._keys.values()))
+        )
+
+    def add(self, key):
+        key_lower = key.lower()
+        self._set.add(key_lower)
+        self._keys[key_lower] = key
+
+    def discard(self, key):
+        key_lower = key.lower()
+        self._set.discard(key_lower)
+        self._keys.pop(key_lower, None)
+
+    def get_canonical_key(self, key):
+        return self._keys[key.lower()]
