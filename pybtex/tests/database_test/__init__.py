955028f1e2ab80967cf5c0a2800c9587f982521e
Andrey Golovizin <ag@sologoc.com>
2015-08-10 18:47:33 +0200

    Add tests for reading/writing bibliography data from/to strings/bytes

--- pybtex/tests/database_test/__init__.py
+++ pybtex/tests/database_test/__init__.py
@@ -19,60 +19,136 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+from abc import ABCMeta, abstractmethod
 from copy import deepcopy
 from unittest import TestCase
-from io import BytesIO, TextIOWrapper, BufferedWriter
+from io import BytesIO, TextIOWrapper
 import pickle
 
-from .data import reference_data
+from nose.tools import assert_true, assert_equal, assert_is_instance
 
 from pybtex.plugin import find_plugin
 from pybtex.database import parse_string, parse_bytes
 
-class DatabaseIOTest(TestCase):
-    def setUp(self):
+from .data import reference_data
+
+
+class DatabaseIO(object):
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
         self.reference_data = deepcopy(reference_data)
-        self.assertTrue(self.reference_data.entries)
-        self.assertTrue(self.reference_data.preamble)
+        assert_true(reference_data.entries)
+        assert_true(reference_data.preamble)
 
-    def _test_input(self, plugin):
-        parser = find_plugin('pybtex.database.input', plugin)(encoding='UTF-8')
-        writer = find_plugin('pybtex.database.output', plugin)(encoding='UTF-8')
+    @abstractmethod
+    def serialize(self, bib_data):
+        pass
+
+    @abstractmethod
+    def deserialize(self, bib_data):
+        pass
+
+
+class PybtexDatabaseIO(DatabaseIO):
+    def __init__(self, bib_format):
+        super(PybtexDatabaseIO, self).__init__()
+        self.bib_format = bib_format
+        self.writer = find_plugin('pybtex.database.output', bib_format)(encoding='UTF-8')
+        self.parser = find_plugin('pybtex.database.input', bib_format)(encoding='UTF-8')
+
+        if bib_format == 'bibtexml':
+            # BibTeXML does not support TeX preambles
+            self.reference_data._preamble = []
+
+    def __repr__(self):
+        return '{}({!r})'.format(type(self).__name__, self.bib_format)
+
+
+class PybtexStreamIO(PybtexDatabaseIO):
+    def serialize(self, bib_data):
         stream = BytesIO()
-        writer_stream = TextIOWrapper(stream, 'UTF-8') if writer.unicode_io else stream
-        writer.write_stream(self.reference_data, writer_stream)
-        writer_stream.flush()
+        unicode_stream = TextIOWrapper(stream, 'UTF-8')
+        self.writer.write_stream(bib_data, unicode_stream if self.writer.unicode_io else stream)
+        unicode_stream.flush()
         stream.seek(0)
-        if parser.unicode_io:
-            parser.parse_string(stream.getvalue().decode('UTF-8'))
-        else:
-            parser.parse_bytes(stream.getvalue())
-        loaded_data = parser.data
-        self.assertEqual(loaded_data, self.reference_data)
-        self.assertEqual(pickle.loads(pickle.dumps(loaded_data, 0)), self.reference_data)
-        self.assertEqual(pickle.loads(pickle.dumps(loaded_data, 1)), self.reference_data)
-        self.assertEqual(pickle.loads(pickle.dumps(loaded_data, 2)), self.reference_data)
+        return unicode_stream
 
-    def test_bibtex_input(self):
-        self._test_input('bibtex')
+    def deserialize(self, stream):
+        parser_stream = stream if self.parser.unicode_io else stream.buffer
+        return self.parser.parse_stream(parser_stream)
 
-    def test_bibyaml_input(self):
-        self._test_input('bibyaml')
 
-    def test_bibtexml_input(self):
-        # BibTeXML does not support TeX preambles AFAIK
-        self.reference_data._preamble = []
-        self._test_input('bibtexml')
+class PybtexStringIO(PybtexDatabaseIO):
+    def serialize(self, bib_data):
+        result = bib_data.to_string(self.bib_format)
+        assert_is_instance(result, unicode)
+        return result
 
-    def test_repr(self):
+    def deserialize(self, string):
+        return parse_string(string, self.bib_format)
+
+
+class PybtexBytesIO(PybtexDatabaseIO):
+    def serialize(self, bib_data):
+        result = bib_data.to_bytes(self.bib_format)
+        assert_is_instance(result, str)
+        return result
+
+    def deserialize(self, string):
+        return parse_bytes(string, self.bib_format)
+
+
+class PickleIO(DatabaseIO):
+    def __init__(self, protocol):
+        super(PickleIO, self).__init__()
+        self.protocol = protocol
+
+    def __repr__(self):
+        return '{}(protocol={!r})'.format(type(self).__name__, self.protocol)
+
+    def serialize(self, bib_data):
+        return pickle.dumps(bib_data, protocol=self.protocol)
+
+    def deserialize(self, pickled_data):
+        return pickle.loads(pickled_data)
+
+
+class ReprEvalIO(DatabaseIO):
+    def __repr__(self):
+        return '{}()'.format(type(self).__name__)
+
+    def serialize(self, bib_data):
+        return repr(bib_data)
+
+    def deserialize(self, repr_value):
         from pybtex.utils import OrderedCaseInsensitiveDict
         from pybtex.database import BibliographyData
         from pybtex.database import Entry, Person
-        data_repr = repr(self.reference_data)
-        data = eval(data_repr, {
+        return eval(repr_value, {
             'OrderedCaseInsensitiveDict': OrderedCaseInsensitiveDict,
             'BibliographyData': BibliographyData,
             'Entry': Entry,
             'Person': Person,
         })
-        self.assertEqual(data, self.reference_data)
+
+
+def check_database_io(io_obj):
+    serialized_data = io_obj.serialize(io_obj.reference_data)
+    deserialized_data = io_obj.deserialize(serialized_data)
+    assert_equal(deserialized_data, io_obj.reference_data)
+
+
+def test_database_parsing_and_writing():
+    for io_cls in PybtexStreamIO, PybtexStringIO, PybtexBytesIO:
+        for bib_format in 'bibtex', 'bibtexml', 'yaml':
+            yield check_database_io, io_cls(bib_format)
+
+
+def test_database_pickling():
+    for protocol in range(0, pickle.HIGHEST_PROTOCOL + 1):
+        yield check_database_io, PickleIO(protocol)
+
+
+def test_database_repr():
+    yield check_database_io, ReprEvalIO()
