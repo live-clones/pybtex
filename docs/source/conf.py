de095573c31c20b3225e75940b5d885bd193adeb
Andrey Golovizin <ag@sologoc.com>
2015-09-28 17:50:41 +0200

    Move Sphinx theme to docs/theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -105,7 +105,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'theme'
-html_theme_path = ['.']
+html_theme_path = ['..']
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
