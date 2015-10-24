f878129d8cea19712056fb458d85c29bba3f3849
Andrey Golovizin <ag@sologoc.com>
2015-10-24 12:38:37 +0200

    Add Python intersphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,8 +36,13 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.todo',
     'sphinx.ext.ifconfig',
+    'sphinx.ext.intersphinx',
     'pybtex_doctools.pygments',
 ]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
