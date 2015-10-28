8f93329582e747589a5bed2ffd8ebac0b03ae95e
Andrey Golovizin <ag@sologoc.com>
2015-10-28 11:19:44 +0100

    Remove permalinks from the site page

--- docs/site/conf.py
+++ docs/site/conf.py
@@ -115,6 +115,7 @@ html_context = {
     'site': True,
 }
 html_style = 'site.css'
+html_add_permalinks = ''
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
