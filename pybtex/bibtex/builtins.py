c5efd9d825875a0b0696137de39c108b1df10dc6
Andrey Golovizin <ag@sologoc.com>
2017-08-15 10:15:04 +0200

    Use six.unichr()

--- pybtex/bibtex/builtins.py
+++ pybtex/bibtex/builtins.py
@@ -27,6 +27,8 @@ from __future__ import print_function, unicode_literals
 
 from functools import update_wrapper
 
+import six
+
 import pybtex.io
 from pybtex.bibtex import utils
 from pybtex.bibtex.exceptions import BibTeXError
@@ -206,7 +208,7 @@ def if_(i):
 def int_to_chr(i):
     n = i.pop()
     try:
-        char = unichr(n)
+        char = six.unichr(n)
     except ValueError:
         raise BibTeXError('%i passed to int.to.chr$', n)
     i.push(char)
