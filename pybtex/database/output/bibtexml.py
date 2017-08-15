11d90703851e94c762553a2683258dd1306f0cd1
Andrey Golovizin <ag@sologoc.com>
2017-08-15 10:10:31 +0200

    Replace unicode with six.text_type

--- pybtex/database/output/bibtexml.py
+++ pybtex/database/output/bibtexml.py
@@ -25,6 +25,8 @@ import io
 from xml.sax.saxutils import XMLGenerator
 from xml.sax.xmlreader import AttributesImpl
 
+import six
+
 from pybtex.database.output import BaseWriter
 
 
@@ -93,7 +95,7 @@ class Writer(BaseWriter):
         >>> from pybtex.database import BibliographyData
         >>> data = BibliographyData()
         >>> unicode_xml = Writer().to_string(data)
-        >>> isinstance(unicode_xml, unicode)
+        >>> isinstance(unicode_xml, six.text_type)
         True
         >>> print(unicode_xml)
         <bibtex:file xmlns:bibtex="http://bibtexml.sf.net/">
