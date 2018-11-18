76f329bfb1eb4569ccb3de29a16b7b2b98587664
Andrey Golovizin <ag@sologoc.com>
2018-11-18 13:58:17 +0100

    Add python_requires to setup.py

--- setup.py
+++ setup.py
@@ -81,6 +81,7 @@ setup(
         'Topic :: Utilities',
     ],
     install_requires=install_requires,
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*',
     packages=find_packages(exclude=['docs']),
     include_package_data=True,
     cmdclass={'sdist': Sdist},
