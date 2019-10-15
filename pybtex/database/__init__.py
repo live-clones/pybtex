d17a8d3d0984045290aa03cc5232f896b4ad11df
Andrey Golovizin <ag@sologoc.com>
2019-10-15 20:50:51 +0200

    Import collection ABCs from collection.abc

--- pybtex/database/__init__.py
+++ pybtex/database/__init__.py
@@ -24,8 +24,10 @@ from __future__ import unicode_literals
 from __future__ import print_function
 import re
 
-from collections import Mapping
-from pybtex.plugin import find_plugin
+try:
+    from collections.abc import Mapping
+except ImportError:
+    from collections import Mapping
 
 import six
 
@@ -38,6 +40,8 @@ from pybtex.richtext import Text
 from pybtex.bibtex.utils import split_tex_string, scan_bibtex_string
 from pybtex.errors import report_error
 from pybtex.py3compat import fix_unicode_literals_in_doctest, python_2_unicode_compatible
+from pybtex.plugin import find_plugin
+
 
 
 class BibliographyDataError(PybtexError):
