9feb8189c7c0a1d22d1deb7fce6132af8fa87629
Andrey Golovizin <ag@sologoc.com>
2025-06-21 14:08:04 +0400

    setup.py: Remove deprecated license classifier

--- setup.py
+++ setup.py
@@ -80,7 +80,6 @@ setup(
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
